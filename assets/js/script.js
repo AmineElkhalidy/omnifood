6d36dd7707fb822bbb206162061e8eb4b9310a95
Amine Elkhalidy <elkhalidyamine.a@gmail.com>
2022-04-01 12:07:28 +0000

    Important changes have been added...

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,34 @@ btnNav.addEventListener("click", () => {
   header.classList.toggle("nav-open");
 });
 
+// Smooth Scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    // Getting the href value
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const section = document.querySelector(href);
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      header.classList.toggle("nav-open");
+    }
+  });
+});
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
